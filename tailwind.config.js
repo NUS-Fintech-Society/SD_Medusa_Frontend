93f882dcbee4e3a359955a8f8a8d71d96b93b257
jieqiboh <jieqiboh5836@gmail.com>
2023-10-12 12:31:57 +0800

    Added DM Sans font and different font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,23 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontSize: {
+      'h1-s': '1.875rem',
+      'h1-l': '3.125rem',
+      'h2-s': '1.625rem',
+      'h2-l': '2.625rem',
+      'h3-s': '1.375rem',
+      'h3-l': '2.125rem',
+      'h4-s': '1.125rem',
+      'h4-l': '1.625rem',
+      'body-s': '0.75rem',
+      'body-l': '1.0rem',
+      'button-s': '0.625rem',
+      'button-l': '0.875rem',
+    },
+  },
+  fontFamily: {
+    dmsans: ['var(--font-dmsans)'],
   },
   plugins: [],
 };
