967694ae09976c82769fb2139c6b5c1ab7040504
mingyuanc <cmingyuan123@gmail.com>
2023-10-17 21:27:15 +0800

    Add progress bar component

---
+++ app/mytest/components/ProgressBar.tsx
@@ -0,0 +1,16 @@
+interface ProgressBarProps {
+  completion: Number;
+}
+
+function ProgressBar(ProgressBarProps: ProgressBarProps) {
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+      <div
+        className="bg-blue-600 h-2.5 rounded-full"
+        style={{ width: `${ProgressBarProps.completion}%` }}
+      ></div>
+    </div>
+  );
+}
+
+export default ProgressBar;
