d18f105acd31ae87d000b907a89be695c3cb51a4
Ming Yuan <cmingyuan123@gmail.com>
2023-11-04 22:06:09 +0800

    Add form submit

--- app/contact-us/components/HookForm.tsx
+++ app/contact-us/components/HookForm.tsx
@@ -3,51 +3,45 @@ import { FormControl, Input } from "@chakra-ui/react";
 import Button from "../../components/inputs/SmallButton";
 
 export default function HookForm() {
-  //   const {
-  //     handleSubmit,
-  //     register,
-  //     formState: { errors, isSubmitting },
-  //   } = useForm()
-
-  //   function onSubmit(values: any) { // leave as any for now till finalize
-  //     return new Promise<void>((resolve) => {
-  //       setTimeout(() => {
-  //         alert(JSON.stringify(values, null, 2))
-  //         resolve()
-  //       }, 3000)
-  //     })
-  //   }
-
   return (
     <div className="w-[1006px] h-[399px] text-[24px] font-normal rounded-[20px] bg-white">
-      <form className="flex flex-col items-center justify-evenly h-full px-[44.5px] text-black">
+      <form
+        className="flex flex-col items-center justify-evenly h-full px-[44.5px] text-black"
+        target="_blank"
+        action="https://formsubmit.co/d3aac6a2bd9d8f0b7a20b48015211b62"
+        method="POST"
+      >
         <div className="flex justify-between items h-[54px] w-full">
-          <FormControl isRequired>
-            <Input
-              placeholder="Full Name"
-              className="bg-[#E8E7E7] rounded-[10px] w-[434px] h-full outline-none pl-[20px] caret-black "
-            />
-          </FormControl>
-          <FormControl isRequired>
-            <Input
-              placeholder="Email Address"
-              type="email"
-              className="bg-[#E8E7E7] rounded-[10px] w-[434px] h-full outline-none pl-[20px] caret-black"
-            />
-          </FormControl>
-        </div>
-        <FormControl isRequired>
-          <Input
-            placeholder="Subject"
-            className="bg-[#E8E7E7] rounded-[10px] w-[917px] h-[54px] outline-none pl-[20px] caret-black"
+          <input
+            type="text"
+            name="fullName"
+            placeholder="Full Name"
+            className="bg-[#E8E7E7] rounded-[10px] w-[434px] h-full outline-none pl-[20px] caret-black "
+            required
           />
-        </FormControl>
-        <FormControl isRequired>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            className="bg-[#E8E7E7] rounded-[10px] w-[434px] h-full outline-none pl-[20px] caret-black"
+            required
+          />
+        </div>
+        <input
+          type="text"
+          name="subject"
+          placeholder="Subject"
+          className="bg-[#E8E7E7] rounded-[10px] w-[917px] h-[54px] outline-none pl-[20px] caret-black"
+          required
+        />
+        <div className="form-group">
           <textarea
+            name="message"
             placeholder="Message"
             className="bg-[#E8E7E7] rounded-[10px] w-[917px] h-[142px] outline-none pl-[20px] caret-black resize-none"
+            required
           />
-        </FormControl>
+        </div>
         <div className="w-[138px] h-[28px] text-[13px]">
           <Button colour="brand-yellow-black">Send Message</Button>
         </div>
